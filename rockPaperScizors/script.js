c86736d3c67ca85c953b16da9bc12fe4ff1e3b2f
Forloski <70533448+Forloski@users.noreply.github.com>
2020-09-19 14:30:56 -0300

    Fixed identing problems in RockPaperScizors

--- rockPaperScizors/script.js
+++ rockPaperScizors/script.js
@@ -1,84 +1,79 @@
+/* eslint-disable require-jsdoc */
 console.log('start');
 
 let playerPoints = 0;
 let cpuPoints = 0;
 
-let playerScoreText = document.getElementById('playerPoints');
-let cpuScoreText = document.getElementById('cpuPoints');
-let roundResultText = document.getElementById('roundResult');
+const playerScoreText = document.getElementById('playerPoints');
+const cpuScoreText = document.getElementById('cpuPoints');
+const roundResultText = document.getElementById('roundResult');
+const btnTesoura = document.getElementById('btnTesoura');
+const btnPedra = document.getElementById('btnPedra');
+const btnPapel = document.getElementById('btnPapel');
 
 addListeners();
 
 const moves = ['pedra', 'papel', 'tesoura'];
 
-function computerPlay(){
-
-    return moves[Math.floor(Math.random()*3)];
-
+function computerPlay() {
+  return moves[Math.floor(Math.random()*3)];
 }
 
-function playerWinsRound(){
+function playerWinsRound() {
+  playerPoints++;
 
-    playerPoints++;
+  roundResultText.innerHTML = `Resultado do Round: JOGADOR VENCEU`;
+  playerScoreText.innerHTML = `Pontos do Jogador: ${playerPoints}`;
 
-    roundResultText.innerHTML = `Resultado do Round: JOGADOR VENCEU`
-    playerScoreText.innerHTML = `Pontos do Jogador: ${playerPoints}`;
-    
-    if (cpuPoints === 5) {
-        alert("Jogador venceu!.");
-        location.reload();
-    }
+  if (cpuPoints === 5) {
+    alert('Jogador venceu!.');
+    location.reload();
+  }
 }
 
-function cpuWinsRound(){
+function cpuWinsRound() {
+  cpuPoints++;
 
-    cpuPoints++;
+  roundResultText.innerHTML = `Resultado do Round: CPU VENCEU`;
+  cpuScoreText.innerHTML = `Pontos do CPU: ${cpuPoints}`;
 
-    roundResultText.innerHTML = `Resultado do Round: CPU VENCEU`;
-    cpuScoreText.innerHTML = `Pontos do CPU: ${cpuPoints}`;
-
-    if (cpuPoints === 5) {
-        alert("Computador venceu!.");
-        location.reload();
-    }
+  if (cpuPoints === 5) {
+    alert('Computador venceu!.');
+    location.reload();
+  }
 }
 
-function tieRound(){
-    roundResultText.innerHTML = `Resultado do Round: EMPATE`;
+function tieRound() {
+  roundResultText.innerHTML = `Resultado do Round: EMPATE`;
 }
 
-function playRound(playerHand, cpuHand){
+function playRound(playerHand, cpuHand) {
+  console.log(cpuHand);
 
-    console.log(cpuHand)
-
-    if(playerHand == cpuHand){ 
-        tieRound();
-        return 'Empate';
-    }
-    else if(playerHand == 'pedra' && cpuHand == 'tesoura' || playerHand == 'tesoura' && cpuHand == 'papel' || playerHand == 'papel' && cpuHand == 'pedra') {
-        playerWinsRound();
-        return 'Jogandor venceu';
-    }  
-    else {
-        cpuWinsRound();
-        return 'Computador venceu';
-    }
-        
-
+  if (playerHand == cpuHand) {
+    tieRound();
+    return 'Empate';
+  } else if (playerHand == 'pedra' && cpuHand == 'tesoura' ||
+             playerHand == 'tesoura' && cpuHand == 'papel' ||
+             playerHand == 'papel' && cpuHand == 'pedra') {
+    playerWinsRound();
+    return 'Jogandor venceu';
+  } else {
+    cpuWinsRound();
+    return 'Computador venceu';
+  }
 }
 
-function addListeners(){
+function addListeners() {
+  btnPedra.addEventListener('click', function(e) {
+    console.log(playRound('pedra', computerPlay()));
+  });
 
-    btnPedra.addEventListener('click', function (e) {
-        console.log(playRound('pedra', computerPlay()));
-    });
+  btnTesoura.addEventListener('click', function(e) {
+    console.log(playRound('tesoura', computerPlay()));
+  });
 
-    btnTesoura.addEventListener('click', function (e) {
-        console.log(playRound('tesoura', computerPlay()));
-    });
-
-    btnPapel.addEventListener('click', function (e) {
-        console.log(playRound('papel', computerPlay()));
-    });
-
+  btnPapel.addEventListener('click', function(e) {
+    console.log(playRound('papel', computerPlay()));
+  });
 }
