226e41fa8a31960837792b41bc6441a6307bc953
Forloski <70533448+Forloski@users.noreply.github.com>
2020-10-12 23:04:07 -0300

    Learning React

--- project_todolist/src/index.js
+++ project_todolist/src/index.js
@@ -2,63 +2,168 @@
 /* eslint-disable require-jsdoc */
 
 class Todos {
-  constructor(title, description, dueDate, priority, notes, checklist) {
+  constructor(title, description, dueDate, priority, notes, checklist, id, project) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.notes = notes;
     this.checklist = checklist;
+    this.id = id;
+    this.project = project;
   }
 }
 
 class Projects {
-  constructor(title, priority) {
+  constructor(title, id) {
     this.title = title;
-    this.priority = priority;
-    this.todoList = [];
+    this.taskList = [];
+    this.id = title;
   }
 }
 
 function initialPage() {
-  let projectList = JSON.parse(localStorage.projectList);
+  //let list = [];
+  //localStorage.clear();
+  //localStorage.projectList = JSON.stringify(list);
+  const projectList = JSON.parse(localStorage.projectList);
   console.log(projectList);
 
+  populateProjectList();
   createTaskForm();
+  createProjectForm();
   eventListeners();
+}
+
+function populateProjectList() {
+  const projectList = JSON.parse(localStorage.projectList);
+
+  projectList.map((project) => createProjectToHTML(project));
+}
+
+function createTaskToHTML(task) {
+  const taskContainer = document.getElementById('todosContainer');
+  const taskDiv = document.createElement('div');
+
+  taskDiv.innerHTML = `
+    <button type="button" class="collapsible" id="task${task.id}">${task.title}</button>
+    <div class="content">
+      <p> ${task.description} - 
+      ${task.priority} -
+      ${task.dueDate}</p>
+    </div>`;
+
+  taskContainer.appendChild(taskDiv);
+
+  const taskBtn = document.getElementById(`task${task.id}`);
+  collapsible(taskBtn);
+}
+
+function createProjectToHTML(project) {
+  const projectContainer = document.getElementById('projectsContainer');
+  const projectDiv = document.createElement('div');
+
+  projectDiv.innerHTML = `
+    <button type="button" class="collapsible" id="${project.title}">${project.title}</button>
+  `;
+
+  projectContainer.appendChild(projectDiv);
+
+  const projectBtn = document.getElementById(`${project.title}`);
+  projectActive(projectBtn);
 }
 
 function insertTask() {
   document.getElementById('formBackground').style.display = 'block';
-  document.getElementById('formContainer').style.display = 'block';
+  document.getElementById('taskFormContainer').style.display = 'block';
 }
 
 function closeTask() {
   document.getElementById('formBackground').style.display = 'none';
-  document.getElementById('formContainer').style.display = 'none';
+  document.getElementById('taskFormContainer').style.display = 'none';
+}
+
+function projectStorageSize(project) {
+  return project.taskList.length;
+}
+
+function localStorageSize() {
+  const projectList = JSON.parse(localStorage.projectList);
+  return projectList.length;
 }
 
 function submitTask() {
   const task = new Todos;
 
-  task.title = document.getElementById('taskDescInput').value;
+  task.title = document.getElementById('taskTitleInput').value;
   task.description = document.getElementById('taskDescInput').value;
   task.dueDate = document.getElementById('taskDateInput').value;
   task.priority = document.querySelector('input[name="priority"]:checked').value;
   task.checklist = 'false';
 
-  pushTaskLocalStorage(task);
+  clearSubmitTaskBtn();
+  createTaskToHTML(task);
+  pushTaskToProject(task);
 }
 
-function pushTaskLocalStorage(task) {
+function clearSubmitTaskBtn() {
+  document.getElementById('taskTitleInput').value = '';
+  document.getElementById('taskDescInput').value = '';
+  document.getElementById('taskDateInput').value = '';
+}
+
+function pushTaskToProject(task) {
   const projectList = JSON.parse(localStorage.projectList);
-  projectList.push(task);
+  const activeProject = projectList.find((project) => 
+    project.title === checkActiveProject());
+  activeProject.push(task);
   localStorage.projectList = JSON.stringify(projectList);
 }
+
+function insertProject() {
+  document.getElementById('formBackground').style.display = 'block';
+  document.getElementById('projectFormContainer').style.display = 'block';
+}
+
+function closeProject() {
+  document.getElementById('formBackground').style.display = 'none';
+  document.getElementById('projectFormContainer').style.display = 'none';
+}
+
+function submitProject() {
+  const project = new Projects;
+
+  project.title = document.getElementById('projectTitleInput').value;
+
+  clearSubmitProjectBtn();
+  createProjectToHTML(project);
+  pushProjectLocalStorage(project);
+}
+
+function clearSubmitProjectBtn() {
+  document.getElementById('projectTitleInput').value = '';
+}
+
+function pushProjectLocalStorage(project) {
+  const projectList = JSON.parse(localStorage.projectList);
+  projectList.push(project);
+  localStorage.projectList = JSON.stringify(projectList);
+}
+
 
 function eventListeners() {
   insertProjectsBtn.addEventListener('click', function(e) {
     insertProject();
+    console.log(e);
+  });
+
+  btnCloseProject.addEventListener('click', function(e) {
+    closeProject();
+    console.log(e);
+  });
+
+  btnSubmitProject.addEventListener('click', function(e) {
+    submitProject();
     console.log(e);
   });
 
@@ -78,8 +183,26 @@ function eventListeners() {
   });
 }
 
+function createProjectForm() {
+  const container = document.getElementById('projectFormContainer');
+  container.innerHTML = `
+  <div class="formGroup" id="title">
+    <h4>ADD PROJECT</h4>
+    <a class="btnClose" id="btnCloseProject" href="#">&#10005</a>
+    <hr class="new1">
+  </div>
+  <div class="formGroup" id="projectTitleContainer">
+    <label for='projectTitleInput'>Project Title</label>
+    <input type='text' id="projectTitleInput">
+  </div>
+  <div class="formGroup" id="submitPrjBtnContainer">
+      <a class="btnClose" id="btnSubmitProject" href="#">SUBMIT PJCT</a>
+  </div>
+  `;
+}
+
 function createTaskForm() {
-  const container = document.getElementById('formContainer');
+  const container = document.getElementById('taskFormContainer');
   container.innerHTML = `
     <div class="formGroup" id="title">
       <h4>ADD TASK TO PROJECT</h4>
@@ -114,6 +237,34 @@ function createTaskForm() {
     <div class="formGroup" id="submitBtnContainer">
       <a class="btnClose" id="btnSubmitTask" href="#">SUBMIT TASK</a>
     </div>`;
+}
+
+function collapsible(element) {
+  element.addEventListener('click', function() {
+    element.classList.toggle('active');
+    const content = element.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+}
+
+function projectActive(element) {
+  element.addEventListener('click', function() {
+    const projectActive = Array.from(document.getElementsByClassName('projectActive'));
+
+    projectActive.map((project) => project.classList.toggle('projectActive'));
+
+    element.classList.toggle('projectActive');
+  });
+}
+
+function checkActiveProject() {
+  const projectActive = Array.from(document.getElementsByClassName('projectActive'));
+
+  return projectActive[0].innerHTML;
 }
 
 initialPage();
